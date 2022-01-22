9c2be1b580e3d20a2c2889d1b113ef790ad24c35
Manikanta401 <veeruvenkat282@gmail.com>
2022-01-22 21:37:25 +0530

    hii

---
+++ dbo.Enrollment.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Enrollment] ON
+INSERT INTO [dbo].[Enrollment] ([EnrollmentId], [Grade], [CourseId], [StudentId]) VALUES (1, NULL, 1, 1002)
+SET IDENTITY_INSERT [dbo].[Enrollment] OFF
